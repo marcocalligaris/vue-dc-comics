b7a7c15d0de68a3627aca2c5b6cf6696b027b0e2
marcocalligaris <marcocally@gmail.com>
2022-07-24 11:25:43 +0200

    Added Main

---
+++ src/components/MainItem.vue
@@ -0,0 +1,37 @@
+<template>
+<main>
+    <section id="generic-content">
+        <div class="container">
+            <h1>--> Content goes here</h1>
+        </div>
+    </section>
+    <section id="links-section">
+        <div class="container">
+            <nav>
+                <ul>
+                    <li></li>
+                </ul>
+            </nav>
+        </div>
+    </section>
+</main>
+</template>
+
+<script>
+export default {
+    name: 'MainItem',
+}
+</script>
+
+<style scoped>
+    main {
+        color: white;
+    }
+    #generic-content {
+        padding: 40px 0;
+    }
+    #links-section {
+        height: 120px;
+        background-color: #0282F9;
+    }
+</style>
