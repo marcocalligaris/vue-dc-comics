88e07d70cfa43b89d0ecc8162a04f4ec39656986
marcocalligaris <marcocally@gmail.com>
2022-07-24 11:26:04 +0200

    Generic settings

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,24 @@
 <template>
   <div>
-    <header>
-      <H1>DC COMICS</H1>
-    </header>
-    <main></main>
+    <HeaderItem />
+    <MainItem />
     <footer></footer>
   </div>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue'
-
-// export default {
-//   name: 'DcComics',
-//   components: {
-//     
-//   }
-// }
+import HeaderItem from './components/HeaderItem.vue';
+import MainItem from './components/MainItem.vue';
+export default {
+  name: 'DcComics',
+  components: {
+    HeaderItem,
+    MainItem,
+  },
+}
 </script>
 
 <style>
-
 /* Generics */
   * {
     margin: 0;
@@ -30,5 +28,27 @@
 
   body {
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
+    min-height: 100vh;
+    background-color: black;
+    padding: 50px 0;
+  }
+
+  img {
+    max-width: 100%;
+    height: 60px;
+  }
+
+  ul {
+    list-style-type: none;
+  }
+
+  a {
+    color: black;
+    text-decoration: none;
+  }
+
+  .container {
+    margin: 0 auto;
+    max-width: 1300px;
   }
 </style>
