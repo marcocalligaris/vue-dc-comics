c58a2149fa2dc221cc8db5cd655d0745e192badc
marcocalligaris <marcocally@gmail.com>
2022-07-26 15:01:39 +0200

    Added banner

---
+++ src/components/BannerItem.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BannerItem',
+    data() {
+        
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
