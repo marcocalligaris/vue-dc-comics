1452cda1d637fea3c5d924b7287091890e295ea3
marcocalligaris <marcocally@gmail.com>
2022-08-18 12:33:36 +0200

    Banner completed

--- src/components/BannerItem.vue
+++ src/components/BannerItem.vue
@@ -1,18 +1,36 @@
 <template>
-    <div>
-
+    <div class="container" id="banner-gallery">
+        <div class="banner-card" v-for="(item, index) in bannerMenu" :key="index">
+            <a :href="item.url">
+                <img :src="item.src" :alt="item.text">
+                <p>{{ item.text }}</p>
+            </a>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'BannerItem',
-    data() {
-        
+    props: {
+        bannerMenu: Array,
     }
 }
 </script>
 
 <style lang="scss" scoped>
+#banner-gallery {
+    padding: 40px 0;
+    display: flex;
+    justify-content: space-between;
+    .banner-card a {
+        display: flex;
+        align-items: center;
+        p {
+            margin-left: 10px;
+            color: white;
+        }
+    }
+}
 
 </style>
