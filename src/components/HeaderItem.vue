dfb27d66fb6d49732a5978dce13bb89541703ead
marcocalligaris <marcocally@gmail.com>
2022-08-18 12:34:08 +0200

    Header changed

--- src/components/HeaderItem.vue
+++ src/components/HeaderItem.vue
@@ -22,21 +22,8 @@
 <script>
 export default {
     name: 'HeaderItem',
-    data(){
-        return {
-            links:[
-                {id:1, url:'#', text:'CHARACTERS', active: false},
-                {id:2, url:'#', text:'COMICS', active: false},
-                {id:3, url:'#', text:'MOVIES', active: false},
-                {id:4, url:'#', text:'TV', active: false},
-                {id:5, url:'#', text:'GAMES', active: false},
-                {id:6, url:'#', text:'COLLECTIBLES', active: false},
-                {id:7, url:'#', text:'VIDEOS', active: false},
-                {id:8, url:'#', text:'FANS', active: false},
-                {id:9, url:'#', text:'NEWS', active: false},
-                {id:10, url:'#', text:'SHOP', active: false},
-            ]
-        }
+    props: {
+        links: Array,
     }
 }
 </script>
