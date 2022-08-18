b710a94ab11911d746b50da1803f1d74866ad8cb
marcocalligaris <marcocally@gmail.com>
2022-08-18 12:33:54 +0200

    Footer completed

--- src/components/FooterItem.vue
+++ src/components/FooterItem.vue
@@ -6,44 +6,31 @@
                     <div>
                         <h3>DC COMICS</h3>
                         <ul>
-                            <li><a href="#">Characters</a></li>
-                            <li><a href="#">Comics</a></li>
-                            <li><a href="#">Movies</a></li>
-                            <li><a href="#">TV</a></li>
-                            <li><a href="#">Games</a></li>
-                            <li><a href="#">Videos</a></li>
-                            <li><a href="#">News</a></li>
+                            <li v-for="(item, index) in footerDcComicsMenu" :key="index">
+                            <a :href="item.url">{{ item.text }}</a>
+                            </li>
                             <h3>SHOP</h3>
                             <ul>
-                                <li><a href="#">Shop DC</a></li>
-                                <li><a href="#">Shop DC Collectibles</a></li>
+                                <li v-for="(item, index) in footerShopMenu" :key="index">
+                            <a :href="item.url">{{ item.text }}</a>
+                            </li>
                             </ul>
                         </ul>
                     </div>
                     <div>
                         <h3>DC</h3>
                         <ul>
-                            <li><a href="#">Terms Of Use</a></li>
-                            <li><a href="#">Privacy policy (New)</a></li>
-                            <li><a href="#">Ad Choices</a></li>
-                            <li><a href="#">Advertising</a></li>
-                            <li><a href="#">Jobs</a></li>
-                            <li><a href="#">Subscriptions</a></li>
-                            <li><a href="#">Talent Workshops</a></li>
-                            <li><a href="#">CPSC Certificates</a></li>
-                            <li><a href="#">Ratings</a></li>
-                            <li><a href="#">Shop Help</a></li>
-                            <li><a href="#">Contact Us</a></li>
+                            <li v-for="(item, index) in footerDcMenu" :key="index">
+                            <a :href="item.url">{{ item.text }}</a>
+                            </li>
                         </ul>
                     </div>
                     <div>
                         <h3>SITES</h3>
                         <ul>
-                            <li><a href="#">DC</a></li>
-                            <li><a href="#">MAD Magazine</a></li>
-                            <li><a href="#">DC Kids</a></li>
-                            <li><a href="#">DC Universe</a></li>
-                            <li><a href="#">DC Power Visa</a></li>
+                            <li v-for="(item, index) in footerSitesMenu" :key="index">
+                            <a :href="item.url">{{ item.text }}</a>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -59,11 +46,11 @@
                     <div id="social-media">
                         <h1>FOLLOW US</h1>
                         <ul>
-                            <li><a href="#"><img src="../assets/img/footer-facebook.png" alt="Facebook"></a></li>
-                            <li><a href="#"><img src="../assets/img/footer-twitter.png" alt="Facebook"></a></li>
-                            <li><a href="#"><img src="../assets/img/footer-youtube.png" alt="Facebook"></a></li>
-                            <li><a href="#"><img src="../assets/img/footer-pinterest.png" alt="Facebook"></a></li>
-                            <li><a href="#"><img src="../assets/img/footer-periscope.png" alt="Facebook"></a></li>
+                            <li v-for="(item, index) in footerSocialMenu" :key="index">
+                                <a :href="item.url">
+                                    <img :src="(item.src)" :alt="item.text">
+                                </a>
+                            </li>
                         </ul>
                     </div>
                 </nav>
@@ -75,60 +62,58 @@
 <script>
 export default {
     name: 'FooterItem',
+    props: {
+        footerDcComicsMenu: Array,
+        footerShopMenu: Array,
+        footerDcMenu: Array,
+        footerSitesMenu: Array,
+        footerSocialMenu: Array,
+    },
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
 footer {
     color: white;
     height: 600px;
-}
-
+    ul {
+        list-style-type: none;
+    };
+};
 .footer-top {
     background-image: url('../assets/img/footer-bg.jpg');
     background-repeat: no-repeat;
     background-size: cover;
     height: 80%;
-}
-
-footer ul {
-    list-style-type: none;
-}
-
-.footer-top #dc-world {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 100%;
-}
-
-.footer-top #dc-world .footer-col-left {
-    display: flex;
-}
-
-.footer-col-left h3 {
-    margin: 15px 0;
-}
-
-.footer-col-left ul li {
-    margin-right: 25px;
-}
-
-.footer-col-left ul a {
-    color: #878E78;
-}
-
-.footer-top #dc-world .footer-col-right {
-    position: relative;
-    height: 100%;
-}
-
-.footer-col-right img {
-    height: 120%;
-    position: relative;
-    bottom: -50%;
-    transform: translateY(-50%);
-}
+    #dc-world {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        height: 100%;
+        .footer-col-left {
+            display: flex;
+            h3 {
+                margin: 15px 0;
+            };
+            ul li {
+                margin-right: 25px;
+            };
+            ul a {
+                color: #878E78;
+            };
+        };
+        .footer-col-right {
+            position: relative;
+            height: 100%;
+            img {
+                height: 120%;
+                position: relative;
+                bottom: -50%;
+                transform: translateY(-50%);
+            };
+        };
+    };
+};
 .footer-bottom {
     background-color: #303030;
     padding: 30px 0;
@@ -137,34 +122,32 @@ footer ul {
     left: 0;
     right: 0;
     bottom: 0;
-}
-.footer-bottom nav {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-}
-.footer-bottom nav button {
-    border: solid 2px #0282F9;
-    background-color: transparent;
-    color: white;
-    padding: 10px 20px;
-    height: 60px;
-}
-
-#social-media h1 {
-    font-size: 2rem;
-    color:#0282F9;
-}
+    nav {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        button {
+            border: solid 2px #0282F9;
+            background-color: transparent;
+            color: white;
+            padding: 10px 20px;
+            height: 60px;
+            cursor: pointer;
+        };
+    };
+};
 .footer-bottom #social-media {
     display: flex;
     align-items: center;
-}
-
-#social-media ul {
-    display: flex;
-}
-
-#social-media ul li {
-    margin-left: 20px;
-}
+    h1 {
+        font-size: 2rem;
+        color:#0282F9;
+    };
+    ul {
+        display: flex;
+        li {
+            margin-left: 20px;
+        };
+    };
+};
 </style>
